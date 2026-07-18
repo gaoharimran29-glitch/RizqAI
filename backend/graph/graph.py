41e4b27c48ea212bbfbc58f50741eca94ed4415f
Gaohar Imran <gaoharimran29@gmail.com>
2026-07-18 11:50:40 +0530

    Fixed imports

--- backend/graph/graph.py
+++ backend/graph/graph.py
@@ -1,6 +1,6 @@
 from langgraph.graph import START, END, StateGraph
-from graph.state import GraphState
-from agents.planner_agent import planner_agent
+from .state import GraphState
+from backend.agents.planner_agent import planner_agent
 
 graph_builder = StateGraph(GraphState)
 
@@ -9,4 +9,4 @@ graph_builder.add_node("planner_agent", planner_agent)
 graph_builder.add_edge(START, "planner_agent")
 graph_builder.add_edge("planner_agent", END)
 
-graph_builder.compile()
+final_graph = graph_builder.compile()
