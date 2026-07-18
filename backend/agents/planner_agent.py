0d38afe9387adff5bd436d8e3ae2550db4b08794
Gaohar Imran <gaoharimran29@gmail.com>
2026-07-18 11:20:20 +0530

    refactored the code

--- backend/agents/planner_agent.py
+++ backend/agents/planner_agent.py
@@ -1,18 +1,12 @@
 from config import llm
-
 from graph.state import GraphState
-
 from prompts.prompts import PLANNER_PROMPT
-
 from schemas.planner_state import PlannerState
 
 planner_llm = llm.with_structured_output(PlannerState)
 
-
 def planner_agent(state: GraphState) -> GraphState:
-    """
-    Planner Agent
-    """
+    """Planner Agent"""
 
     query = state["user_query"]
 
--- backend/schemas/planner_state.py
+++ backend/schemas/planner_state.py
@@ -1,9 +1,6 @@
 from typing import List
-
 from pydantic import BaseModel, Field
 from enum import Enum
-
-
 class Intent(str, Enum):
     STOCK_ANALYSIS = "stock_analysis"
     STOCK_PRICE = "stock_price"
@@ -16,19 +13,8 @@ class Intent(str, Enum):
     UNRELATED = "unrelated"
 
 class PlannerState(BaseModel):
-    """
-    Structured output returned by the Planner Agent.
-    """
+    """Structured output returned by the Planner Agent."""
 
-    intent: Intent = Field(
-        description="Classified intent of the user's query."
-    )
-
-    company: str | None = Field(
-        default=None,
-        description="The company or stock symbol if present."
-    )
-
-    tasks: List[str] = Field(
-        description="List of agent names to execute."
-    )
+    intent: Intent = Field(description="Classified intent of the user's query.")
+    company: str | None = Field(default=None, description="The company or stock symbol if present.")
+    tasks: List[str] = Field(description="List of agent names to execute.")
